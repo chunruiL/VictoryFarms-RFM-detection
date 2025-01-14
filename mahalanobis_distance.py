7e0ddedf88c1361d29bf9771229aec97ad8a458d
Name <19cl51@queensu.ca>
2025-01-14 00:42:55 -0500

    Change

--- mahalanobis_distance.py
+++ mahalanobis_distance.py
@@ -63,30 +63,26 @@ def flag_outlier_customers(rfm_movement_transformed, timestamps, flag_start, cri
 
     
 
-def flag_outlier_customers_revised(rfm_movement_transformed, timestamps, flag_start, critical_value=7.81):
+def flag_outlier_customers(rfm_movement_transformed, critical_value=7.81):
     """
     Flag outlier customers based on their Mahalanobis Distance of monthly change in RFM
-    to the mean change. The flagging process begins from the month of the flag_start
-    parameter. 
+    to the mean change. The flagging process will be applied to every element.
 
     Parameters:
     - rfm_movement_transformed (list of DataFrame): List of DataFrames with transformed Mahalanobis distances.
-    - timestamps (DatetimeIndex): Timestamps corresponding to each month in the data.
-    - flag_start(string): the string 
     - critical_value (float): Critical value for Mahalanobis Distance to flag outliers. Default is 7.81.
 
     Returns:
-    - md_customers_flagged (list of list): List of lists containing indices of outlier customers for each month after June 2022.
+    - md_customers_flagged (list of list): List of lists containing indices of outlier customers for each detection.
     """
     md_customers_flagged = []
 
-    for time, current_month_rfm in zip(timestamps, rfm_movement_transformed):
+    for i, current_month_rfm in enumerate(rfm_movement_transformed):
         outlier_customers = current_month_rfm[(current_month_rfm['Mahalanobis_Distance']**2 > critical_value) 
                                               & (current_month_rfm['frequency'] < 0)
                                               & (current_month_rfm['monetary_value'] < 0)].index
 
-        if time >= pd.Timestamp(flag_start):
-            md_customers_flagged.append(outlier_customers.to_list())
-            print(f"{time.strftime('%Y-%m')}: {outlier_customers.size} outliers flagged")
+        md_customers_flagged.append(outlier_customers.to_list())
+        print(f"Detection {i+1}: {len(outlier_customers)} outliers flagged")
 
     return md_customers_flagged
